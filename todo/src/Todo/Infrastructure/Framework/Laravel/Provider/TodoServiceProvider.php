7f8f6b9edc768f0b92bfb8501a80a11e194ebf85
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-12-17 08:24:18 +0100

    Laravel app

---
+++ todo/src/Todo/Infrastructure/Framework/Laravel/Provider/TodoServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Todo\Infrastructure\Provider;
+
+use Illuminate\Support\ServiceProvider;
+
+class TodoServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
